f005fcd26eced347cbdfd44328e572f8a1c0dcae
Bodie-Zwank <bodenzwank@gmail.com>
2024-12-23 22:22:01 -0600

    Initial file structure

---
+++ README.md
@@ -0,0 +1,3 @@
+This is the website for the Social Coffee Club.
+
+It currently is comprised solely of HTML, CSS, and JS
---
+++ js/main.js
@@ -0,0 +1,16 @@
+// For implementing header html on main page
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('partials/heading.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
