b608b85263272346c7a9db671c40dc17201dd7ca
Bodie-Zwank <bodenzwank@gmail.com>
2025-01-02 16:18:00 -0600

    footer gets added to main page

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error('There was a problem with the fetch operation:', error);
         });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('partials/footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
